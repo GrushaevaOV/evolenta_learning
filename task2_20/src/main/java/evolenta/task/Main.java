0c86edd99bfbd36aba9e319ca07dc0023f798b6c
GrishaevaOV <olga.gr2812@mail.ru>
2023-08-14 18:57:24 +0300

    2.20

--- task2_20/src/main/java/evolenta/task/Main.java
+++ task2_20/src/main/java/evolenta/task/Main.java
@@ -1,6 +1,8 @@
 package evolenta.task;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +10,29 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 15) + 1;
         }
-        String intAString = Arrays.toString(arr);
-        System.out.println(intAString);
+        for (int i : arr) System.out.print(i + " ");
+        printDoubleNum(arr);
+    }
+
+    public static HashMap<Integer, Integer> doubleNum(int[] array) {
+        HashMap<Integer, Integer> doubleNum = new HashMap<>();
+        for (int j : array) {
+            if (!doubleNum.containsKey(j)) {
+                doubleNum.put(j, 1);
+            } else {
+                doubleNum.put(j, doubleNum.get(j) + 1);
+            }
+        }
+        return doubleNum;
+    }
+
+    public static void printDoubleNum(int[] array) {
+        HashMap<Integer, Integer> doubleNum = new HashMap<>(doubleNum(array));
+        System.out.println();
+        for (Map.Entry<Integer, Integer> entryMap : doubleNum.entrySet()) {
+            if (entryMap.getValue() > 1) {
+                System.out.println("число " + entryMap.getKey() + " встречается " + entryMap.getValue() + " раза");
+            }
+        }
     }
 }
