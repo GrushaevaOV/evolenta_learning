36446ad03232d21214706702135115b058a70fdb
GrishaevaOV <olga.gr2812@mail.ru>
2023-08-14 19:15:17 +0300

    2.20

--- task2_20/src/main/java/evolenta/task/Main.java
+++ task2_20/src/main/java/evolenta/task/Main.java
@@ -1,26 +1,25 @@
 package evolenta.task;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[20];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (Math.random() * 15) + 1;
+        int[] nums = new int[20];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (int) (Math.random() * 15) + 1;
         }
-        for (int i : arr) System.out.print(i + " ");
-        printDoubleNum(arr);
+        for (int i : nums) System.out.print(i + " ");
+        printDoubleNum(nums);
     }
 
     public static HashMap<Integer, Integer> doubleNum(int[] array) {
         HashMap<Integer, Integer> doubleNum = new HashMap<>();
-        for (int j : array) {
-            if (!doubleNum.containsKey(j)) {
-                doubleNum.put(j, 1);
+        for (int i : array) {
+            if (!doubleNum.containsKey(i)) {
+                doubleNum.put(i, 1);
             } else {
-                doubleNum.put(j, doubleNum.get(j) + 1);
+                doubleNum.put(i, doubleNum.get(i) + 1);
             }
         }
         return doubleNum;
