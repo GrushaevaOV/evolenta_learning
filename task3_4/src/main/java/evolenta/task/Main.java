27a7e016f3882235d209c01e1f4c1fc06babbba7
GrishaevaOV <olga.gr2812@mail.ru>
2023-08-15 10:48:59 +0300

    3.4 done

--- task3_4/src/main/java/evolenta/task/Main.java
+++ task3_4/src/main/java/evolenta/task/Main.java
@@ -1,7 +1,25 @@
 package evolenta.task;
 
+import evolenta.task.models.User;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Введите имя");
+        Scanner in = new Scanner(System.in);
+        String name1 = in.nextLine();
+        System.out.println("Введите возраст");
+        int age1 = in.nextInt();
+        User user1 = new User(name1,age1);
+        Scanner in2 = new Scanner(System.in);
+        System.out.println("Введите имя");
+        String name2 = in2.nextLine();
+        System.out.println("Введите возраст");
+        int age2 = in2.nextInt();
+        User user2 = new User(name2,age2);
+        if (user1.getAge()<user2.getAge()) System.out.println(user1.toString());
+        else System.out.println(user2.toString());
+
     }
 }
