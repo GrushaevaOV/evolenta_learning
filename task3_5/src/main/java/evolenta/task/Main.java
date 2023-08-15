2a3caaf1204914afa66f55147e7e7b77a9807035
GrishaevaOV <olga.gr2812@mail.ru>
2023-08-15 11:15:12 +0300

    3.5 done

--- task3_5/src/main/java/evolenta/task/Main.java
+++ task3_5/src/main/java/evolenta/task/Main.java
@@ -1,7 +1,23 @@
 package evolenta.task;
 
+import evolenta.task.models.User;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<User> users = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите имя");
+            String name = in.nextLine();
+            System.out.println("Введите возраст");
+            int age = in.nextInt();
+            users.add(new User(name, age));
+        }
+        Collections.sort(users);
+        for (User user : users) {
+            System.out.println(user.toString());
+        }
     }
 }
