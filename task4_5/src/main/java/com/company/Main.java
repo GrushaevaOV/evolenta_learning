b0bd6b69e668b9428e7be57217bb348de0df7444
GrishaevaOV <olga.gr2812@mail.ru>
2023-08-16 16:48:37 +0300

    4_5 done

--- task4_5/src/main/java/com/company/Main.java
+++ task4_5/src/main/java/com/company/Main.java
@@ -1,7 +1,13 @@
 package com.company;
 
+import java.util.stream.LongStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(sum(10000000,1000000000));
+    }
+
+    public static Long sum (int a, int b){
+        return LongStream.range(a,b).sum();
     }
 }
